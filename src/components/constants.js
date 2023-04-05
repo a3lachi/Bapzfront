fa120443108addb6635af9415382a5a1b59da83c
a3laxi <mad.aalachi@gmail.com>
2023-04-05 09:48:44 +0200

    💫💫💫

--- src/components/constants.js
+++ src/components/constants.js
@@ -6,4 +6,4 @@
 export const Proxy = 'https://bapzend.vercel.app'
 
 
-export const Local = window.location.hostname === 'localhost' ? `http://${window.location.host}` : window.location.host
+export const Local = window.location.hostname === 'localhost' ? `http://${window.location.host}` : `https://${window.location.host}` 
